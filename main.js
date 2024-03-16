e638f8dd8fd2b0b475a0beac21ffed54b4479c6d
haseebshahbaz <haseebshahbazpk786@gmail.com>
2024-03-16 08:33:30 +0500

    First Ten Question
    Successfully Solved the First Ten Question

---
+++ main.js
@@ -0,0 +1,27 @@
+//45 Question Assignment of Governor Initiative for Artificial Intelligance
+// Question No 2
+var personName = "Kamil Khan";
+console.log("Assalam u alikum ".concat(personName, " how are you?"));
+// Question No 4
+console.log("Allama Iqbal once said, \"Words without power, is mere philosophy\"");
+//Question No 5
+var authorName = "Allama Iqbal";
+var message = "Words without power, is mere philosophy";
+console.log("".concat(authorName, " once said, \"").concat(message, "\""));
+//Question No 7
+//Addition Operator
+console.log("Addition: 4 + 4 = ".concat(4 + 4));
+//Subtraction Operator
+console.log("Subtraction: 9 - 1 = ".concat(9 - 1));
+//Multiplication Operator
+console.log("Multiplication: 2 * 4 = ".concat(2 * 4));
+//Division Operator
+console.log("Division: 64 / 8 = ".concat(64 / 8));
+//Question No 8
+console.log(4 + 4);
+console.log(16 - 8);
+console.log(4 * 2);
+console.log(16 / 2);
+//Question No 9
+var favNum = 7;
+console.log("Your favorite number is \"".concat(favNum, "\""));
---
+++ main.ts
@@ -0,0 +1,44 @@
+//45 Question Assignment of Governor Initiative for Artificial Intelligance
+
+
+// Question No 2
+let personName: string = "Kamil Khan"
+
+console.log(`Assalam u alikum ${personName} how are you?`)
+
+// Question No 4
+console.log(`Allama Iqbal once said, "Words without power, is mere philosophy"`)
+
+//Question No 5
+let authorName: string = "Allama Iqbal"
+let message: string = "Words without power, is mere philosophy"
+
+console.log(`${authorName} once said, "${message}"`)
+
+//Question No 7
+
+//Addition Operator
+console.log(`Addition: 4 + 4 = ${4 + 4}`)
+
+//Subtraction Operator
+console.log(`Subtraction: 9 - 1 = ${9 - 1}`)
+
+//Multiplication Operator
+console.log(`Multiplication: 2 * 4 = ${2 * 4}`)
+
+//Division Operator
+console.log(`Division: 64 / 8 = ${64 / 8}`)
+
+
+//Question No 8
+console.log(4 + 4)
+console.log(16 - 8)
+console.log(4 * 2)
+console.log(16 / 2)
+
+
+//Question No 9
+let favNum: number = 7
+
+console.log(`Your favorite number is "${favNum}"`)
+
